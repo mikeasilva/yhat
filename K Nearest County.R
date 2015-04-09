76ba288a6977ca37cce5f9bb63147930307ac99e
Mike Silva <mike.a.silva@gmail.com>
2015-04-09 13:46:56 -0400

    Updated K Nearest County

--- K Nearest County.R
+++ K Nearest County.R
@@ -2,45 +2,59 @@
 ## Written by: Mike Silva
 
 ## Description:
-## Using the 2010 Geographic Identifiers downloaded from American FactFinder, 
-## this function will find the closest counties to a given lattitude and 
-## longitude.
+## Using R county map, this function will find the k closest counties to a 
+## given lattitude and longitude and return the name, fips, and distance.
 
 ## Example:
 ## To get the 3 nearest counties for Rochester, NY
-## df <- data.frame(k=3, lat=43.1656, lon=77.6114)
-## kncounty(df)
+## df <- data.frame(k=3, lat=43.1656, lon=-77.6114)
+## kcounty(df)
 
+library(ggplot2)
 library(dplyr)
 library(fields)
 
-csv <- read.csv('DEC_10_DP_G001_with_ann.csv') %>%
-  select(GEO.display.label, GEO.id2, VD074, VD075)
-names(csv) <- c('county.name', 'fips', 'lat', 'lon')
-csv <- csv[2:nrow(csv),] # remove the meta information
-
-counties <- csv %>%
-  mutate(lat = as.numeric(as.character(lat))) %>%
-  mutate(lon = - as.numeric(as.character(lon))) %>%
-  select(lat, lon)
-
-fips <- csv %>%
-  select(fips)
-
-county.names <- csv %>%
-  select(county.name)
-
-rm(csv)
+data <- read.csv('counties.csv', colClass=rep('character',3)) %>%
+  merge(., map_data('county')) %>%
+  mutate(lat=as.numeric(lat)) %>%
+  mutate(lon=as.numeric(long)) %>%
+  rename(fips=county.fips) %>%
+  select(county.name,fips,lat,lon)
 
 ## Input is data frame with
 ## lon: longitude
 ## lat: latitude
 ## k: number of counties nearest neighbors
-kncounty <- function(df){
-  coords <- df[, c('lat', 'lon')]
-  distance <- as.data.frame(rdist.earth(counties, coords))
-  return.df <- data.frame(fips = fips, name = county.names, distance = distance)
-  names(return.df) <- c('fips', 'name','distance')
-  return.df <- return.df[order(return.df$distance),]
-  return.df[1:df$k,]
+kcounty <- function(df){
+  data$distance <- as.vector(rdist.earth(data[,c('lat','lon')], df[, c('lat', 'lon')]))
+  data <-aggregate(data[ , c('distance')]  ,list(data$fips, data$county.name) , min)
+  names(data) <- c('fips', 'name','distance')
+  data <- data[order(data$distance),]
+  data[1:df$k,]
 }
+
+## Deploy function on yhat
+
+install.packages('yhatr')
+library(yhatr)
+
+model.require <- function() {
+  library(fields)
+}
+
+model.transform <- function(df) {
+  df
+}
+
+model.predict <- function(df) {
+  kcounty(df)
+}
+
+## Username/API key go here!
+yhat.config  <- c(
+  username='YOUR_USERNAME',
+  apikey='YOUR_API_KEY',
+  env='http://cloud.yhathq.com/'
+)
+
+yhat.deploy('kcounty')
